ba1856d410b7c7c0de4c80370c2836ab7b9098b9
Víctor Alonso <works.alonsog@gmail.com>
2019-08-26 10:32:14 +0200

    Nuevo rol para reservas

--- wp-content/themes/ltdeh/includes/cpt/book.php
+++ wp-content/themes/ltdeh/includes/cpt/book.php
@@ -28,14 +28,52 @@ function ltdeh_custom_post_type_book() {
         'show_ui' => true,
         'query_var' => true,
         'rewrite' => array('slug' => 'reservas', 'with_front' => true),
-        'capability_type' => 'post',
         'hierarchical' => false,
         'menu_position' => null,
+        'show_admin_column' => true,
         'supports' => array('title', 'page-attributes'),
-        'has_archive' => false
+        'has_archive' => false,
+        'capabilities' => array(
+            'read_post' => 'read_book',
+            'read_private_posts' => 'read_private_book',
+            'edit_post' => 'edit_book',
+            'edit_posts' => 'edit_books',
+            'edit_others_posts' => 'edit_others_books',
+            'edit_published_posts' => 'edit_published_books',
+            'edit_private_posts' => 'edit_private_books',
+            'delete_post' => 'delete_book',
+            'delete_posts' => 'delete_books',
+            'delete_others_posts' => 'delete_others_books',
+            'delete_published_posts' => 'delete_published_books',
+            'delete_private_posts' => 'delete_private_books',
+            'publish_posts' => 'publish_books',
+        ),
     ];
 
     register_post_type('book', $args);
 }
 
 add_action('after_setup_theme', 'ltdeh_custom_post_type_book');
+
+function ltdeh_add_custom_caps() {
+    $roles = array('subscriber', 'administrator');
+    foreach($roles as $role){
+        $role = get_role( $role );
+        $role->add_cap( 'read' );
+        $role->add_cap( 'read_book');
+        $role->add_cap( 'read_private_books' );
+        $role->add_cap( 'edit_book' );
+        $role->add_cap( 'edit_books' );
+        $role->add_cap( 'edit_others_books' );
+        $role->add_cap( 'edit_published_books' );
+        $role->add_cap( 'edit_private_books' );
+        $role->add_cap( 'delete_book');
+        $role->add_cap( 'delete_books');
+        $role->add_cap( 'delete_others_books');
+        $role->add_cap( 'delete_published_books' );
+        $role->add_cap( 'delete_private_books' );
+        $role->add_cap( 'publish_books' );
+    }
+}
+
+add_action( 'admin_init', 'ltdeh_add_custom_caps');
