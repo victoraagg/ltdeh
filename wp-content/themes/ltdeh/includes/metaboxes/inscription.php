5c17d6bacd3d92e64dfb9a08414ff55b37c7523b
Víctor Alonso <works.alonsog@gmail.com>
2019-08-29 12:10:01 +0200

    Contador de inscritos

--- wp-content/themes/ltdeh/includes/metaboxes/inscription.php
+++ wp-content/themes/ltdeh/includes/metaboxes/inscription.php
@@ -19,7 +19,7 @@ add_action('add_meta_boxes_inscription', 'ltdeh_custom_metabox_inscription');
 function ltdeh_custom_metabox_inscription_callback($post, $data) {
     $postMeta = get_post_meta($post->ID, 'inscription_members', true);
     $members = unserialize($postMeta);
-    echo '<h1>Listado de inscritos</h1>';
+    echo '<h1>Listado de inscritos ('.count($members).')</h1>';
     if(!empty($members)){
         foreach ($members as $key => $member) {
             foreach ($member as $value) {
