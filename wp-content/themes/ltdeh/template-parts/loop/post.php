11679a8bfabe25483ab3a9f92f909ef15434acf6
Victor Alonso Gomez <works.alonsog@gmail.com>
2020-03-26 15:01:45 +0100

    Paginación de archivos

--- wp-content/themes/ltdeh/template-parts/loop/post.php
+++ wp-content/themes/ltdeh/template-parts/loop/post.php
@@ -3,6 +3,16 @@ if ( have_posts() ) :
 	while ( have_posts() ) : the_post();
 		get_template_part( 'template-parts/post/content', 'loop' );
 	endwhile;
+	$args = array(
+		'show_all' => false,
+		'end_size' => 1,
+		'mid_size' => 2,
+		'prev_next' => true,
+		'prev_text' => __('<', 'commseek'),
+		'next_text' => __('>', 'commseek'),
+		'type' => 'list',
+	);
+	the_posts_pagination($args);
 else :
 	get_template_part( 'template-parts/post/content', 'none' );
 endif;
